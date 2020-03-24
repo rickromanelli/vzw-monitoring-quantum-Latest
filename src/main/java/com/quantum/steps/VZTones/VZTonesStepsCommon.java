66e0535b4a3c0f47cdbec02d94ceb3ec9eb4b2d6
rickr <rickr@nb-rickr-10.nexperience.com>
2020-03-24 14:14:21 -0400

    initiate Tones back to basics

--- src/main/java/com/quantum/steps/VZTones/VZTonesStepsCommon.java
+++ src/main/java/com/quantum/steps/VZTones/VZTonesStepsCommon.java
@@ -39,11 +39,8 @@ public class VZTonesStepsCommon {
 		
 		PerfectoApplicationSteps.waitFor(30);
 
-		we = (QAFExtendedWebElement) driver.findElement("home.signin");
-		PerfectoApplicationSteps.waitFor(30);
-		if(we.isPresent()) {
-			we.click();
-		}
+
+		
 	}
 
 	
