20e9853daea02652b3a9b69a2f2340256d41771a
rickr <rickr@nb-rickr-10.nexperience.com>
2021-12-15 10:13:06 -0500

    initial 922

---
+++ src/main/java/com/quantum/steps/Emergency/EmergencySteps.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package com.quantum.steps.Emergency;
+
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.qmetry.qaf.automation.core.ConfigurationManager;
+import com.qmetry.qaf.automation.step.QAFTestStepProvider;
+import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebDriver;
+import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
+import com.quantum.steps.PerfectoApplicationSteps;
+import com.quantum.steps.CommonSteps.PerfectoGenericSteps;
+import com.quantum.utils.DeviceUtils;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.it.Date;
+
+@QAFTestStepProvider
+public class EmergencySteps extends WebDriverTestCase {
+	QAFExtendedWebDriver driver = new WebDriverTestBase().getDriver();
+	QAFExtendedWebElement we;
+	PerfectoGenericSteps pgs = new PerfectoGenericSteps();
+
+
+	@Given("^I send a text to NineTwoTwo")
+	public void sendText922() throws Exception {
+
+		
+		try {
+			PerfectoApplicationSteps.closeAppByName("Messages");
+			PerfectoApplicationSteps.closeAppByName("Messages");
+		} catch (Exception ex) {
+
+		}
+		PerfectoApplicationSteps.startAppByName("Messages");
+		
+		pgs.textCheckpoint("Conversations", "30", "90");
+		
+		we = (QAFExtendedWebElement) driver.findElement("messages.newMessage");
+		we.waitForPresent(60000);
+		we.click();
+		
+		
+
+			
+			
+			we = (QAFExtendedWebElement) driver.findElement("messages.recipientFrame");
+			we.waitForPresent(60000);
+			//we.click();
+			
+			//we.sendKeys("922");
+			
+			Map<String, Object> params5 = new HashMap<>();
+			params5.put("label", "Recipient");
+			params5.put("text", "922");
+			//params5.put("screen.top", "0%");
+			//params5.put("screen.height", "34%");
+			//params5.put("screen.left", "0%");
+			//params5.put("screen.width", "100%");
+			Object result5 = getDriver().executeScript("mobile:edit-text:set", params5);
+			
+			
+			we = (QAFExtendedWebElement) driver.findElement("messages.messageField");
+			we.waitForPresent(60000);
+			we.click();
+			
+			Map<String, Object> params6 = new HashMap<>();
+			params6.put("label", "Enter Message");
+			params6.put("text", "test");
+			//params5.put("screen.top", "0%");
+			//params5.put("screen.height", "34%");
+			//params5.put("screen.left", "0%");
+			//params5.put("screen.width", "100%");
+			Object result6 = getDriver().executeScript("mobile:edit-text:set", params6);
+			
+			
+			//we.sendKeys("test");
+			
+			if (pgs.textFind("test", "10", "100")) {
+			
+			
+			
+			we = (QAFExtendedWebElement) driver.findElement("messages.sendButton");
+			we.waitForPresent(60000);
+			we.click();
+			
+		
+			
+		//PUBLIC:TextDeniedCircle.png
+
+	}
+
+	}
+	
+	@Then("^I delete 922 messages")
+	public void iDelete922Messages() throws Exception {
+
+		we = (QAFExtendedWebElement) driver.findElement("messages.menu");
+		we.waitForPresent(60000);
+		we.click();
+		
+		we = (QAFExtendedWebElement) driver.findElement("messages.deleteMessages");
+		we.waitForPresent(60000);
+		we.click();
+		
+		we = (QAFExtendedWebElement) driver.findElement("messages.selectAll2Delete");
+		we.waitForPresent(60000);
+		we.click();
+		
+		
+		pgs.textClick("Delete", "20", "100");
+		
+		we = (QAFExtendedWebElement) driver.findElement("messages.move2trashConfirm");
+		we.waitForPresent(60000);
+		we.click();
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/main/resources/scenarios/Emergency/emergency.feature
@@ -0,0 +1,9 @@
+@Emergency
+Feature: Emergency
+#Sample Test Scenario Description
+
+@Emergency
+Scenario: Emergency_922_SMS
+	Then I send a text to NineTwoTwo 
+	Then Transaction "922 call success" description: "code exists" SLA: "6000" - OCR Checkpoint: "RC=0" Timeout: "60" Threshold: "100"
+	Then I delete 922 messages 
